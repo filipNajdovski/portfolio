c550fee9a2539d5fa457032fd04af1d988897fd0
filipNajdovski <filip-najdovski@live.com>
2024-07-18 17:23:25 +0200

    imported json file for globe in intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import HyperGlobe from "./components/HyperGlobe"
+import profilePic from "./../images/filip.jpg"
 
 export default function Home() {
   return (
@@ -9,8 +10,27 @@ export default function Home() {
           <HyperGlobe />
         </section>
 
+        <section className="portfolio p-8">
+          <div className="profile-picture">
+          <Image
+            src={profilePic} // Path to your image
+            alt="Filip Najdovski"
+            width={150} // Desired width
+            height={150} // Desired height
+          />
+          </div>
+          <div className="title">
+            <h2>Filip Najdovski</h2>
+            <br />
+            <h3>Front End Developer</h3>
+          </div>
+          <div className="summary"></div>
+          <div className="experience"></div>
+          <div className="education"></div>
+        </section>
+
         <section className="keypoints">
-            
+          
         </section>
 
         <section className="projects">
