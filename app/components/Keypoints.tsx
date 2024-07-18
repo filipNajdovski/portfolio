bc4e95f8641580f809488311c829e31c66d13250
filipNajdovski <filip-najdovski@live.com>
2024-07-19 00:37:20 +0200

    added keypoints needs work

---
+++ app/components/Keypoints.tsx
@@ -0,0 +1,191 @@
+const Keypoints = () => {
+  return (
+    <>
+    <div className="key-points-container">
+    <h2 className="key-points-showcase__title overflow-hidden" style="--delay: 0s">
+      <span className="has-in-common to-top"><span className="heading-lines">Key</span><span className="heading-regular"> Points</span></span>
+    </h2>
+    <div className="key-points-showcase__technologies-container">
+
+      <ul className="key-points-showcase__technologies">
+        <a href="#">
+          <li onclick="alterIcons('web-apps')" className="key-points-showcase__technology has-in-common to-top" style="--delay: 0.2s">
+            <span><img className="key-points-showcase__link" src="/assets/media/images/link.svg" alt="link"></span><span>Development - Web Applications</span>
+          </li>
+        </a>
+        <a href="#">
+          <li onclick="alterIcons('mobile-apps')" className="key-points-showcase__technology has-in-common to-top" style="--delay: 0.4s">
+            <span><img className="key-points-showcase__link" src="/assets/media/images/link.svg" alt="link"></span><span>Development - Mobile Apps</span>
+          </li>
+        </a>
+        <a href="#">
+          <li onclick="alterIcons('websites')" className="key-points-showcase__technology has-in-common to-top" style="--delay: 0.6s">
+            <span><img className="key-points-showcase__link" src="/assets/media/images/link.svg" alt="link"></span><span>Development - Websites</span>
+          </li>
+        </a>
+        <a href="#">
+          <li onclick="alterIcons('social-media-marketing')" className="key-points-showcase__technology has-in-common to-top" style="--delay: 0.8s">
+            <span><img className="key-points-showcase__link" src="/assets/media/images/link.svg" alt="link"></span><span>Social Media Marketing</span>
+          </li>
+        </a>
+        <a href="#">
+          <li onclick="alterIcons('graphic-design')" className="key-points-showcase__technology has-in-common to-top" style="--delay: 1s">
+            <span><img className="key-points-showcase__link" src="/assets/media/images/link.svg" alt="link"></span><span>Graphic Design</span>
+          </li>
+        </a>
+      </ul>
+    </div>
+
+    <div className="key-points-showcase__click-me">
+      <img src="/assets/media/images/arrow.svg" loading="lazy" alt="" className="key-points-showcase__click-me__img to-left has-in-common">
+      <div className="key-points-showcase__click-me__text to-left has-in-common">Click to see Technologies !!! </div>
+    </div>
+    </div>
+
+
+    <div className="shapes to-left has-in-common">
+
+        <div className="row">
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/nodejs.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/html.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/javascript.svg')"></div>
+            </div>
+        </div>
+        </div>
+
+        <div className="row">
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/mysql.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/css.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/flutter.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/drupal.svg')"></div>
+            </div>
+        </div>
+        </div>
+
+        <div className="row">
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/postgresql.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/php.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/codeigniter.svg')"></div>
+            </div>
+        </div>
+        </div>
+
+        <div className="row">
+        <div className="shape transparent-shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style=""></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/docker.svg')"></div>
+            </div>
+        </div>
+        <div className="shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style="--background-image: url('/assets/media/images/technologies/svelte.svg')"></div>
+            </div>
+        </div>
+        <div className="shape transparent-shape" data-offset=".05">
+            <div className="shape__right"></div>
+            <div className="shape__left"></div>
+            <div className="shape__top"></div>
+            <div className="shape__bottom"></div>
+            <div className="shape__center">
+            <div className="image" style=""></div>
+            </div>
+        </div>
+        </div>
+
+    </div>
+  </>
+  )
+}
+
+export default Keypoints
--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,46 @@ export default function Home() {
             <br />
             <h3>Front End Developer</h3>
           </div>
-          <div className="summary"></div>
-          <div className="experience"></div>
-          <div className="education"></div>
+          <div className="summary">
+            <h3>About me</h3>
+            <p>
+            Passionate and dedicated web developer with two years of professional 
+            experience in front-end development, specializing in React.js, with 
+            additional proficiency in Angular and Vue.js. Strong background in both 
+            software and hardware, continually updating my skills with the latest 
+            technologies and development principles. Experienced in backend technol
+            ogies including PHP and Node.js. Eager to contribute to and grow with a 
+            forward-thinking company that embraces cutting-edge coding practices 
+            and technologies.
+            </p>
+          </div>
+          <div className="experience">
+            <h3>Experience</h3>
+            <ul>
+              <li>Web Developer - Freelance</li>
+              <li>Front End Developer - Kimicom</li>
+              <li>Quality Control - Taskforce LLC</li>
+              <li>International Project Coordinator - Media Plus Consulting</li>
+              <li>Marketing Development - Rothstein International</li>
+            </ul>
+          </div>
+          <div className="certificates">
+            <h3>Certificates</h3>
+            <ul>
+              <li>Udemy - React Front to Back</li>
+              <li>Udemy - Modern JavaScript From The Beginning (2024)</li>
+              <li>Udemy - 20 Web Projects with Vanilla JavaScript</li>
+              <li>Codecademy - Full Web Development Path</li>
+              <li>Codecademy - Object Oriented Programming/PHP</li>
+            </ul>
+          </div>
+          <div className="education">
+            <h3>Education</h3>
+            <ul>
+              <li>Computer Science and Communications Engineering - Bachelor's Degree</li>
+              <li>SOTU Gjorgji Naumov - Technician for Computer Science and Automatics</li>
+            </ul>
+          </div>
         </section>
 
         <section className="keypoints">
