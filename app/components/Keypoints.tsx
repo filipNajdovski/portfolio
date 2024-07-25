07f9ee0a6c4accc26914c13519091911a041595a
filipNajdovski <filip-najdovski@live.com>
2024-07-25 18:44:52 +0200

    added tab component

--- app/components/Keypoints.tsx
+++ app/components/Keypoints.tsx
@@ -4,6 +4,7 @@ import HtmlIcon from './../../images/technologies/html.svg'
 import CssIcon from './../../images/technologies/css.svg'
 import GraphqlIcon from './../../images/technologies/graphql.svg'
 import JavaScriptIcon from './../../images/technologies/javascript.svg'
+import TabComponent from "./TabComponent"
 
 
 const Keypoints = () => {
@@ -15,6 +16,7 @@ const Keypoints = () => {
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi aspernatur quis illo voluptas! Incidunt quidem consequatur voluptatibus deleniti vitae tempora unde asperiores mollitia laborum expedita rerum maiores beatae, enim sint.</p>
       </div>
       
+      <TabComponent />
 
       <div className="tech-menu">
         <ul>
