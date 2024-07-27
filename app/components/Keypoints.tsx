9be23d34a3067a08fdd77e2292b1a5e8cf660060
filipNajdovski <filip-najdovski@live.com>
2024-07-27 22:16:26 +0200

    added new backgrounds

--- app/components/Keypoints.tsx
+++ app/components/Keypoints.tsx
@@ -6,8 +6,8 @@ const Keypoints = () => {
 
   return (
     <div>
-      <div className="tech-header">
-        <h2>Technologies</h2>
+      <div className="tech-header p-8">
+        <h2 className="text-[#f0db4f] font-bold text-xl py-3">Technologies</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi aspernatur quis illo voluptas! Incidunt quidem consequatur voluptatibus deleniti vitae tempora unde asperiores mollitia laborum expedita rerum maiores beatae, enim sint.</p>
       </div>
       
--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,23 @@ export default function Home() {
         </section>
 
         <section className="portfolio p-8">
-          <div className="profile-picture">
+          <div className="profile-picture flex justify-center items-center">
           <Image
             src={profilePic} // Path to your image
-            alt="Filip Najdovski"
+            alt="Filip Najdovski Picture"
             width={150} // Desired width
             height={150} // Desired height
+            className="rounded-full border-4 drop-shadow-xl "
           />
           </div>
-          <div className="title">
-            <h2>Filip Najdovski</h2>
+          <div className="title flex flex-col justify-center gap-2 items-center">
+            <h2 className=" text-[#e5bb89] text-2xl font-medium">Filip Najdovski</h2>
             <br />
-            <h3>Front End Developer</h3>
+            <h3 className="text-white text-xl font-bold">Front End Developer</h3>
           </div>
           <div className="summary">
-            <h3>About me</h3>
-            <p>
+            <h3 className="text-[#e5bb89] text-lg font-bold py-3">About me</h3>
+            <p className="text-white text-sm">
             Passionate and dedicated web developer with two years of professional 
             experience in front-end development, specializing in React.js, with 
             additional proficiency in Angular and Vue.js. Strong background in both 
@@ -39,8 +40,8 @@ export default function Home() {
             </p>
           </div>
           <div className="experience">
-            <h3>Experience</h3>
-            <ul>
+            <h3 className="text-[#e5bb89] text-lg font-bold py-3">Experience</h3>
+            <ul className="text-white text-sm">
               <li>Web Developer - Freelance</li>
               <li>Front End Developer - Kimicom</li>
               <li>Quality Control / Project Coordinator - Taskforce LLC</li>
@@ -49,8 +50,8 @@ export default function Home() {
             </ul>
           </div>
           <div className="certificates">
-            <h3>Certificates</h3>
-            <ul>
+            <h3 className="text-[#e5bb89] text-lg font-bold py-3">Certificates</h3>
+            <ul className="text-white text-sm">
               <li>Udemy - React Front to Back</li>
               <li>Udemy - Modern JavaScript From The Beginning (2024)</li>
               <li>Udemy - 20 Web Projects with Vanilla JavaScript</li>
@@ -59,8 +60,8 @@ export default function Home() {
             </ul>
           </div>
           <div className="education">
-            <h3>Education</h3>
-            <ul>
+            <h3 className="text-[#e5bb89] text-lg font-bold py-3">Education</h3>
+            <ul className="text-white text-sm">
               <li>UKLO Computer Science and Communications Engineering - Bachelor's Degree</li>
               <li>SOTU Gjorgji Naumov - Technician for Computer Science and Automatics</li>
             </ul>
