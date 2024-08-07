57a076cb4ad897abad3532248635a4fb528eec82
filipNajdovski <filip-najdovski@live.com>
2024-08-07 18:17:38 +0200

    divided client senction into components

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import GlobeComponent from "./components/GlobeComponent"
 import profilePic from "./../images/filip.jpg"
 import Keypoints from "./components/Keypoints";
+import Testimonials from "./components/Testimonials";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -74,16 +76,8 @@ export default function Home() {
         </section>
 
         <section className="clients p-8">
-            <div className="testimonials">
-              <h3 className=" text-[#e5bb89] text-2xl font-medium py-3">Testimonials</h3>
-              <p>Over the years, I have had the privilege of working on a diverse range of projects, each one presenting unique challenges and opportunities for growth. My journey as a web developer has been shaped by these experiences, and I am proud to showcase some of the projects that have defined my career. The following testimonials from my clients and collaborators provide insight into the quality of my work, my dedication to excellence, and the positive impact I strive to make through my contributions.</p>
-              <div className="testimonial">
-              
-              </div>
-            </div>
-            <div className="projects">
-              <h3 className=" text-[#e5bb89] text-2xl font-medium py-3">Projects</h3>
-            </div>
+          <Testimonials />
+          <Projects />
         </section>
     </main>
   );
