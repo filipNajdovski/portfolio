b4d678428ab4c61b20620017079b8af74b6be742
filipNajdovski <filip-najdovski@live.com>
2024-08-12 15:39:18 +0200

    added changes

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,7 +1,12 @@
+import styles from './Projects.module.css'
+
 const Projects = () => {
   return (
     <div className="projects">
         <h3 className=" text-[#e5bb89] text-2xl font-medium py-3">Projects</h3>
+        <div className={styles.projectsScroll}>
+
+        </div>
     </div>
   )
 }
