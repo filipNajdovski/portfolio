47167f009dd5c049ddcb2a6cf2422d35ccecc4e4
filipNajdovski <filip-najdovski@live.com>
2024-11-23 13:23:43 +0100

    added meta data for better SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,38 @@ import Header from "./components/Header";
 import { Suspense } from "react";
 import Footer from "./components/Footer";
 import Script from "next/script";
-import data from './../data.json'
 import Loading from "./loading";
 
 export const metadata: Metadata = {
   title: "Filip Najdovski - Building Dynamic Websites",
-  description: "Portfolio page",
+  description: "I design and develop modern web apps and websites tailored to your needs, offering seamless UI/UX design and data-driven solutions to elevate your brand and business.",
+  twitter: {
+    card: "summary_large_image",
+    title: "Filip Najdovski - Building Dynamic Websites",
+    description: "I design and develop modern web apps and websites tailored to your needs, offering seamless UI/UX design and data-driven solutions to elevate your brand and business.",
+    images: [
+      {
+        url: "./../images/filip-logo.png",
+        width: 456, // Optional: specify image dimensions
+        height: 555, // Optional: specify image dimensions
+        alt: "Najdovski Technologies and Software Development", // Optional: image alt text
+      },
+    ], // Use an array for the image URL(s)
+  },
+  openGraph: {
+    title: "Filip Najdovski - Building Dynamic Websites",
+    description: "I design and develop modern web apps and websites tailored to your needs, offering seamless UI/UX design and data-driven solutions to elevate your brand and business.",
+    url: "https://www.najdovski.tech/",
+    siteName: "Najdovski Technologies - Building Dynamic Websites",
+    images: [
+      {
+        url: "./../images/filip-logo.png",
+        width: 456, // Optional: specify image dimensions
+        height: 555, // Optional: specify image dimensions
+        alt: "Najdovski Technologies and Software Development", // Optional: image alt text
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
